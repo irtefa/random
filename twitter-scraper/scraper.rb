e796adf3ab59d8dd826e767cdd1de25d7d54186c
Mohd Irtefa <irtefa1@illinois.edu>
2012-12-10 03:08:03 -0600

    added a comment that describes the reason why twitter only gives 20 tweets bback

--- twitter-scraper/scraper.rb
+++ twitter-scraper/scraper.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 require 'json'
 require 'pp'
 url = "http://twitter.com/irtefa1"
-# only gets you 20 tweets. But why?
+# only gets you 20 tweets. Because twitter loads only 20 tweets by default
 doc = Nokogiri::HTML(open(url))
 
 doc.css('.stream-items').each do |node|
